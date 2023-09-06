3e9b38d04fc77dba21c883c846063a2bdf5489ea
ivanprytula <ivanprytula87@gmail.com>
2023-09-06 14:25:38 +0200

    add 403,404,500, account, users templates and urlpatterns

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views import defaults as default_views
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.authtoken.views import obtain_auth_token
 
 # https://adamj.eu/tech/2022/11/24/django-settings-patterns-to-avoid/
 # TIP: Don’t Read Settings at Import Time - Do it at run time!
@@ -29,38 +33,59 @@ from django.urls import include, path
 #  Settings don’t often change, but when they do, you definitely want to use the new value.
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("pages.urls", namespace="pages")),
-    path("__reload__/", include("django_browser_reload.urls")),
+    # Django Admin, use {% url 'admin:index' %}
+    path(settings.ADMIN_URL, admin.site.urls),
+    # User management
+    path("users/", include("price_navigator.users.urls", namespace="users")),
+    path("accounts/", include("allauth.urls")),
+    # Your stuff: custom urls includes go here
+    # ...
+]
+
+# API URLS
+urlpatterns += [
+    # API base url
+    path("api/", include("config.api_router")),
+    # DRF auth token
+    path("auth-token/", obtain_auth_token),
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
 ]
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
-    # urlpatterns += [
-    #     path(
-    #         "400/",
-    #         default_views.bad_request,
-    #         kwargs={"exception": Exception("Bad Request!")},
-    #     ),
-    #     path(
-    #         "403/",
-    #         default_views.permission_denied,
-    #         kwargs={"exception": Exception("Permission Denied")},
-    #     ),
-    #     path(
-    #         "404/",
-    #         default_views.page_not_found,
-    #         kwargs={"exception": Exception("Page not Found")},
-    #     ),
-    #     path("500/", default_views.server_error),
-    # ]
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
 
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
